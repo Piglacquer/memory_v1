f60f7e045b9134793879985cc9b1d6a6dffcf769
Patrick Biffle <pbiffle@gmail.com>
2018-01-22 10:03:39 -0700

    fixed database in knex file

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
-// Update with your config settings.
-
 module.exports = {
 	development: {
 		client: 'pg',
-		connection: 'postgres://localhost/memory_v1'
+		connection: 'postgres:///memory_v1'
 	},
 	production: {
 		client: 'pg',
-		connection: process.env.DATABASE_URL
+		connection:
+			process.env.DATABASE_URL ||
+			'postgres://egskgmehdrptea:3dd16e5d9b459367908b29411da0e1a36c85cbd8b45681cb7553a5cedefdbf0f@ec2-54-235-252-137.compute-1.amazonaws.com:5432/d1djqcekbr9rfp'
 	}
 }
